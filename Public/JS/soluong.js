93a4be4215f487f772eb532b42810eea1a8504b8
dattuanle <dat062004@gmail.com>
2025-06-23 22:51:02 +0700

    Delete Public directory

--- Public/JS/soluong.js
+++
@@ -1,7 +0,0 @@
-function validateQuantity(input) {
-    if (input.value < 0) {
-        input.setCustomValidity('Số lượng không được âm');
-    } else {
-        input.setCustomValidity('');
-    }
-}
